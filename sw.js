315e5d7084c0d24631fa91d92b8d6b9454cb4a07
BuildTools <unconfigured@null.spigotmc.org>
2026-07-15 16:01:32 +0900

    Fix: remove contain:strict and overflow:hidden from math-field to prevent formula clipping; container overflow:hidden clips externally, JS ResizeObserver locks height

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'make10-cache-v136';
+const CACHE_NAME = 'make10-cache-v137';
 const ASSETS_TO_CACHE = [
   './index.html',
-  './css/style.css?v=136',
-  './js/script.js?v=136',
+  './css/style.css?v=137',
+  './js/script.js?v=137',
   './favicon.png',
   './apple-touch-icon.png',
   './manifest.json',
