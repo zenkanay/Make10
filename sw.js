d44acc64d143e4b327866d9ecc05f943e3603f6a
BuildTools <unconfigured@null.spigotmc.org>
2026-07-15 17:19:59 +0900

    style(handwriting): reduce bottom padding of keyboard container on handwriting layout and reduce mobile container height (v158)

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'make10-cache-v157';
+const CACHE_NAME = 'make10-cache-v158';
 const ASSETS_TO_CACHE = [
   './index.html',
-  './css/style.css?v=157',
-  './js/script.js?v=157',
+  './css/style.css?v=158',
+  './js/script.js?v=158',
   './favicon.png',
   './apple-touch-icon.png',
   './manifest.json',
