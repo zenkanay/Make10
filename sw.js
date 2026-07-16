fda9f7aa7831ed69a480a6210d8a189f9bd73bff
BuildTools <unconfigured@null.spigotmc.org>
2026-07-16 14:50:51 +0900

    style(handwriting): change handwriting desmos-keyboard to inline layout relative to app-container to resolve PC and mobile viewport spacing gaps (v166)

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'make10-cache-v165';
+const CACHE_NAME = 'make10-cache-v166';
 const ASSETS_TO_CACHE = [
   './index.html',
-  './css/style.css?v=165',
-  './js/script.js?v=165',
+  './css/style.css?v=166',
+  './js/script.js?v=166',
   './favicon.png',
   './apple-touch-icon.png',
   './manifest.json',
